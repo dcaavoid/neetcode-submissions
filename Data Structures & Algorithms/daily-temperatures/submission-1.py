5b6563d8f0ef4dbc83f6189d270e2739fa8fa939
Zeyuan Cao <80137644+dcaavoid@users.noreply.github.com>
2026-06-30 10:17:20 +0800

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # Use monotonic decreasing stack (store index)
+        stack = []
+        res = [0] * len(temperatures)
+
+        for i in range(len(temperatures)):
+            # If a greater temperature comes, keep popping any temperatures that is smaller in the front.
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                index = stack.pop()
+                res[index] = i - index
+
+            stack.append(i)
+        
+        return res
