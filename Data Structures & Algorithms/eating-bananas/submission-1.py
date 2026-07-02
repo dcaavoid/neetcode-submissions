b204d9b3d73cbc740d4afc489124e407683acf17
Zeyuan Cao <80137644+dcaavoid@users.noreply.github.com>
2026-07-02 11:12:33 +0800

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # The range of k is [1, max(piles[i])], given h >= len(piles).
+        # Use binary search on the range of k:
+        # If mid(k) satisifies the h, try smaller portion of k;
+        # If mid(k) rate takes longer time than h, try larger portion of k.
+
+        left, right = 1, max(piles)
+        res = 0
+
+        while left <= right:
+            mid = (left + right) // 2
+            time = 0
+            
+            # Count the number of hours for current mid k
+            for p in piles:
+                time += math.ceil(p / mid)
+            
+            # If time <= h, try smaller k
+            if time <= h:
+                res = mid   # Why can I substitute this line with res = min(res, mid)
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return res
