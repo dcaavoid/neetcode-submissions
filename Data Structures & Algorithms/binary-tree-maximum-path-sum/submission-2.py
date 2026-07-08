789d1bbd76f644a33c4e553974bb14867621c568
Zeyuan Cao <80137644+dcaavoid@users.noreply.github.com>
2026-07-08 11:20:42 +0800

    Add: binary-tree-maximum-path-sum - submission-2

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-2.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        # Use DFS to search for the max gain from visiting node + left or right subtree.
+        # Don't include negative left or right subtree in the return of recursion.
+        # Use a gloabl variable to keep track the max gain without splitting from node.
+        self.res = root.val
+
+        # Return the max gain from splitting at node.
+        def dfs(node) -> int:
+            # Base case
+            if not node:
+                return 0
+            
+            # Recursive
+            left = max(0, dfs(node.left))
+            right = max(0, dfs(node.right))
+
+            # Check if including both left and right subtress make the max path.
+            self.res = max(self.res, node.val + left + right)
+            return max(left, right) + node.val
+        
+        dfs(root)
+        return self.res
+            
