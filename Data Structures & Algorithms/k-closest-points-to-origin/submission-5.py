f322b8745596bb57a8d429239118e7dc0bcf82d3
Zeyuan Cao <80137644+dcaavoid@users.noreply.github.com>
2026-07-09 10:54:29 +0800

    Add: k-closest-points-to-origin - submission-5

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        # For distance, do not need sqrt b/c if a^2 > b^2, then sqrt(a^2) > sqrt(b^2).
+        # Use a tuple to store (dist, coord) pair.
+        # Use max heap of size k with (-dist, [x, y]).
+        # Pop the current farthest coordinate in O(log(k)) time.
+        # Time: O(n*log(k)) for pushing and popping n elements into maxheap of size k;
+        # Space: O(k) for keep the k closest elements in a max heap.
+        res = []
+        maxHeap = []
+        for x, y in points:
+            dist = (x ** 2) + (y ** 2)
+            heapq.heappush(maxHeap, (-dist, x, y))
+
+            if len(maxHeap) > k:
+                heapq.heappop(maxHeap)
+        
+        for _, x, y in maxHeap:
+            res.append([x, y])
+        
+        return res
