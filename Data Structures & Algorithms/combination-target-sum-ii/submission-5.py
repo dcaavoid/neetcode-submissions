f4cfeef9458679807b4e6ba898c41708dc193329
Zeyuan Cao <80137644+dcaavoid@users.noreply.github.com>
2026-07-10 14:42:41 +0800

    Add: combination-target-sum-ii - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-5.py
@@ -0,0 +1,37 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        # Backtracking with DFS.
+        # Different from Leetcode 39 b/c in Leetcode 39, each number is distinct.
+        # In DFS, two choices:
+        # 1. choose the current element, and go to next elements;
+        # 2. not choose the current element and skip any following duplicate elements.
+        # So need to sort the array first.
+        candidates.sort()
+        res = []
+        curr = []
+
+        def dfs(i, total):
+            # Base case
+            if total == target:
+                res.append(curr.copy())
+                return
+            
+            if i == len(candidates) or total > target:
+                return
+
+            # [1, 2, 2, 3, 5, 6, 9]
+            #  i  n
+            # Recursive
+            # 1. Choose candidates[i]
+            curr.append(candidates[i])
+            dfs(i + 1, total + candidates[i])
+
+            # 2. Skip candidates[i] and any following candidates[i + 1] == candidates[i]
+            curr.pop()
+            while i + 1 < len(candidates) and candidates[i+1] == candidates[i]:
+                i += 1
+            
+            dfs(i + 1, total) # Don't understand why we i + 1 here as we update i?
+         
+        dfs(0, 0)
+        return res
