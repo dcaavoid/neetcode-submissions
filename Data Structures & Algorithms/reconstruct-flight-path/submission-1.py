ed37df3ee7660bfe8a8b6affdf49207c2e5301da
Zeyuan Cao <80137644+dcaavoid@users.noreply.github.com>
2026-07-14 17:24:36 +0800

    Add: reconstruct-flight-path - submission-1

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        # Adjacency list + DFS
+        tickets.sort()
+        adj = {}
+        for src, dst in tickets:
+            if src not in adj:
+                adj[src] = []
+            adj[src].append(dst)
+        
+        res = ['JFK']
+
+        # Return if all tickets can be used starting from
+        def dfs(src):
+            # Base case
+            if len(res) == len(tickets) + 1:
+                return True
+            if src not in adj:
+                return False
+            
+            # Recursive
+            temp = list(adj[src])
+            for i, dst in enumerate(temp):
+                adj[src].pop(i)
+                res.append(dst)
+                if dfs(dst):
+                    return True
+                adj[src].insert(i, dst)
+                res.pop()
+            
+            return False
+        
+        dfs('JFK')
+        return res
