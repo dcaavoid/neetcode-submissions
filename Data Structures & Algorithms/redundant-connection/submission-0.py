dd99fa38e45261f678366ca91b4828fafcbdd906
Zeyuan Cao <80137644+dcaavoid@users.noreply.github.com>
2026-07-14 14:19:28 +0800

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        # Use Union Find to return the first edge that its two nodes have the same root.
+        # By graph theory, a valid graph with n nodes only has n-1 edges, so there must be only one edge.
+        parents = [i for i in range(len(edges) + 1)]
+        rank = [1 for _ in range(len(edges) + 1)]
+
+        # Return the root node of input node.
+        def find(node):
+            root = node
+
+            while root != parents[root]:
+                # Path compression for faster root retrival.
+                parents[root] = parents[parents[root]]
+                root = parents[root]
+            
+            return root
+        
+        # Merge two roots together by ranking.
+        def union(node1, node2):
+            if rank[node1] < rank[node2]:
+                parents[node1] = node2
+                rank[node2] += rank[node1]
+            else:
+                parents[node2] = node1
+                rank[node1] += rank[node2]
+
+        for n1, n2 in edges:
+            p1, p2 = find(n1), find(n2)
+            # Two nodes have the same root -> cycle
+            if p1 == p2:
+                return [n1, n2]
+            else:
+                union(p1, p2)
