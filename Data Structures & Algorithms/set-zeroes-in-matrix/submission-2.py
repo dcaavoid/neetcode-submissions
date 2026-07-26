38dbefd4e6a43bad60809a05de97fff637d61433
Zeyuan Cao <80137644+dcaavoid@users.noreply.github.com>
2026-07-26 16:18:02 +0800

    Add: set-zeroes-in-matrix - submission-2

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-2.py
@@ -0,0 +1,38 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        # first row = True
+        # 1 2 3 4 0
+        # 8 6 8 6 0
+        # 7 2 9 6 1
+        # m = 5, n = 3
+        # Modify in-place b/c we traverse from left to right, top to bottom.
+        # matrix[0][0:n-1] = 0: this column is 0
+        # matrix[1:m-1][0] = 0: this row (except first row) is 0
+        first_row = False
+        m, n = len(matrix), len(matrix[0])
+
+        for row in range(m):
+            for col in range(n):
+                if matrix[row][col] == 0:
+                    if row == 0:
+                        first_row = True
+                    else:
+                        matrix[row][0] = 0
+                    matrix[0][col] = 0
+                    
+        
+        # First set matrix[1:m-1][1:n-1] to 0
+        for row in range(1, m):
+            for col in range(1, n):
+                if matrix[0][col] == 0 or matrix[row][0] == 0:
+                    matrix[row][col] = 0
+        
+        # Then set first row to 0
+        if matrix[0][0] == 0:
+            for row in range(m):
+                matrix[row][0] = 0
+
+        # Lastly set first row to 0 if first_row is True
+        if first_row:
+            for col in range(n):
+                matrix[0][col] = 0
