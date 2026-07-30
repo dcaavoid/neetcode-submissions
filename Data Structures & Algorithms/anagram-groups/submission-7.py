2680ca2b2b7b7ac40f7361d2048a162662c0837c
Zeyuan Cao <80137644+dcaavoid@users.noreply.github.com>
2026-07-30 15:34:29 +0800

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Brute force: for each string of length n, sort each string, and use the sorted string as the key for a hash map.
+        # Time: O(m * nlogn), space: O(m*n)
+        # mapping = defaultdict(list)
+        # for s in strs:
+        #     key = "".join(sorted(s))
+        #     mapping[key].append(s)
+        # return list(mapping.values())
+
+        # Optimal solution: use an tuple of occurrence of each character as the key instead.
+        # Optimize O(nlogn) to O(n) for each string
+        mapping = defaultdict(list)   # [array of length 26]: [list of words with this occurrence]
+        base = ord("a")
+        for s in strs:
+            key = [0] * 26
+            for c in s:
+                key[ord(c) - base] += 1
+            mapping[tuple(key)].append(s)
+        return list(mapping.values())
